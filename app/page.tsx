2f505f902c4cf48cf570cf43037c6f79c36c0afe
Max Akimov <maxymus2828@gmail.com>
2025-11-21 19:58:09 +0200

    add some code to home and layout files

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,13 @@
-export default function Home() {}
+import Container from "@/components/Container/Container";
+import Header from "@/components/Header/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Container>
+        <Header />
+      </Container>
+    </>
+  );
+};
+export default Home;
