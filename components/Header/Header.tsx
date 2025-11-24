20de9b96169f3a7b0c5925d531375b76a5a0fb2c
Akimov Max <maxymus2828@gmail.com>
2025-11-24 19:21:32 +0200

    header update

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,26 +1,31 @@
 import Link from "next/link";
 import css from "./Header.module.css";
+import Container from "../Container/Container";
 
 const Header = () => {
   return (
     <header className={css.header}>
-      <Link href="/" aria-label="Home" className={css.logo}>
-        Travel<span className={css.logo_span}>Trucks</span>
-      </Link>
-      <nav aria-label="Main Navigation">
-        <ul className={css.nav_list}>
-          <li className={css.nav_el}>
-            <Link href="/" className={css.nav_link}>
-              Home
-            </Link>
-          </li>
-          <li className={css.nav_el}>
-            <Link href="/catalog" className={css.nav_link}>
-              Catalog
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <Container>
+        <div className={css.header_wrapper}>
+          <Link href="/" aria-label="Home" className={css.logo}>
+            Travel<span className={css.logo_span}>Trucks</span>
+          </Link>
+          <nav aria-label="Main Navigation">
+            <ul className={css.nav_list}>
+              <li className={css.nav_el}>
+                <Link href="/" className={css.nav_link}>
+                  Home
+                </Link>
+              </li>
+              <li className={css.nav_el}>
+                <Link href="/catalog" className={css.nav_link}>
+                  Catalog
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </Container>
     </header>
   );
 };
