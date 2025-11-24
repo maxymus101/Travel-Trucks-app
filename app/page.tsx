e2f86c0ee22b1a31cc94c80a2e458286139d5edd
Akimov Max <maxymus2828@gmail.com>
2025-11-24 19:19:38 +0200

    home page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 import Container from "@/components/Container/Container";
-import Header from "@/components/Header/Header";
+import css from "./page.module.css";
+import Link from "next/link";
 
 const Home = () => {
   return (
-    <>
+    <section className={css.home}>
       <Container>
-        <Header />
+        <div className={css.home_wrapper}>
+          <div className={css.title}>
+            <h1>Campers of your dreams</h1>
+          </div>
+          <div className={css.sub_title}>
+            <h2>You can find everything you want in our catalog</h2>
+          </div>
+          <Link href="/catalog" className={css.link}>
+            View now
+          </Link>
+        </div>
       </Container>
-    </>
+    </section>
   );
 };
 export default Home;
