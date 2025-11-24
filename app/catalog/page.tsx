8bb478f6abb61312f59c4d1b902a94e81266bd77
Akimov Max <maxymus2828@gmail.com>
2025-11-24 19:21:08 +0200

    add code to catalog-page

---
+++ app/catalog/page.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/Container/Container";
+import css from "./page.module.css";
+
+const Catalog = () => {
+  return (
+    <Container>
+      <section className={css.catalog}></section>
+    </Container>
+  );
+};
+export default Catalog;
